3ceec83eded182aed90e0f4bea6052cd1f161245
ggbotbox <oliver.lu@pa.com.sg>
2023-04-02 02:05:59 +0800

    add SvcQueryDto

---
+++ src/main/java/com/pabu5h/evs2/model/SvcQueryDto.java
@@ -0,0 +1,15 @@
+package com.pabu5h.evs2.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+public class SvcQueryDto {
+    private final SvcTokenDto svcTokenDto;
+    private final Object request;
+}
--- src/main/java/com/pabu5h/evs2/model/SvcTokenDto.java
+++ src/main/java/com/pabu5h/evs2/model/SvcTokenDto.java
@@ -2,15 +2,14 @@ package com.pabu5h.evs2.model;
 
 import lombok.*;
 
-@Data
 @Builder
 @Getter@Setter
 @AllArgsConstructor
 public class SvcTokenDto {
-    private String username;
-    private String svcName;
-    private String endpoint;
-    private String scope;
-    private String target;
-    private String operation;
+    private final String username;
+    private final String svcName;
+    private final String endpoint;
+    private final String scope;
+    private final String target;
+    private final String operation;
 }
