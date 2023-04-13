6f8732c479f8e5c3e30166501a2784e581de0cfa
Oliver Lu <pa596bu500@gmail.com>
2023-04-13 06:04:04 +0000

    add Evs2ConsumerTransactionModel

---
+++ src/main/java/com/pabu5h/evs2/model/Evs2ConsumerTransactionModel.java
@@ -0,0 +1,53 @@
+package com.pabu5h.evs2.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter@Setter
+@Builder
+@AllArgsConstructor
+@Table(name = "transaction_log")
+public class Evs2ConsumerTransactionModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "transaction_id")
+    String transactionId;
+    @Column(name = "transaction_timestamp")
+    LocalDateTime transactionTimestamp;
+    @Column(name = "topup_amt")
+    double topupAmt;
+    @Column(name = "gst")
+    double gst;
+    @Column(name = "net_amt")
+    double netAmt;
+    @Column(name = "payment_mode")
+    int paymentMode;
+    @Column(name = "payment_status")
+    int transactionStatus;
+    @Column(name = "currency")
+    String currency;
+    @Column(name = "meter_displayname")
+    String meterDisplayname;
+    @Column(name = "offer_id")
+    int offerId;
+    @Column(name = "response_timestamp")
+    LocalDateTime responseTimestamp;
+    @Column(name = "complete_send_to_backend")
+    boolean completeSendToBackend;
+    @Column(name = "transaction_code")
+    String transactionCode;
+    @Column(name = "payment_channel")
+    int paymentChannel;
+    @Column(name = "transaction_status_rcved")
+    int transactionStatusRcved;
+    @Column(name = "conversion_ratio")
+    double conversionRatio;
+    @Column(name = "audit_number")
+    String auditNumber;
+    @Column(name = "is_dedicated")
+    boolean isDedicated;
+}
