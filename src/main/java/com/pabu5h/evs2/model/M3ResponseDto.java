a8651fa253f1a665bbd0a0eb1cc1737d8d4297d7
Oliver Lu <pa596bu500@gmail.com>
2023-04-17 02:23:42 +0000

    update M3ResponseDto

--- src/main/java/com/pabu5h/evs2/model/M3ResponseDto.java
+++ src/main/java/com/pabu5h/evs2/model/M3ResponseDto.java
@@ -1,13 +1,14 @@
 package com.pabu5h.evs2.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter@Setter
-public class ResponseDto<T> {
+public class M3ResponseDto<T> {
     @JsonInclude(JsonInclude.Include.NON_NULL)
     String message;
 
@@ -22,4 +23,14 @@ public class ResponseDto<T> {
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
     T result;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    String mid;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    String rls;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty("rls_mid")
+    String rlsMid;
 }
