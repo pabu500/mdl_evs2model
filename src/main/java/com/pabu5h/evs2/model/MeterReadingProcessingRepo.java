cf07f458339e4b0da443b89c6ad4e6ee1dd9d457
Oliver Lu <pa596bu500@gmail.com>
2023-04-27 02:12:29 +0000

    add MeterReadingProcessingRepo

---
+++ src/main/java/com/pabu5h/evs2/model/MeterReadingProcessingRepo.java
@@ -0,0 +1,8 @@
+package com.pabu5h.evs2.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MeterReadingProcessingRepo extends CrudRepository<MeterReadingModel, Long> {
+}
