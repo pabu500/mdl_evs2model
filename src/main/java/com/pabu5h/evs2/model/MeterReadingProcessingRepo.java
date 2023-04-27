ca46b27fa47c357044a3b909c9a3d0d552ffb96b
Oliver Lu <pa596bu500@gmail.com>
2023-04-27 03:04:23 +0000

    misc fix

--- src/main/java/com/pabu5h/evs2/model/MeterReadingProcessingRepo.java
+++ src/main/java/com/pabu5h/evs2/model/MeterReadingProcessingRepo.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MeterReadingProcessingRepo extends CrudRepository<MeterReadingModel, Long> {
+public interface MeterReadingProcessingRepo extends CrudRepository<MeterReadingProcessingModel, Long> {
 }
