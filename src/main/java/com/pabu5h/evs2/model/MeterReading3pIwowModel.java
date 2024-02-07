ad84de597977df3ef86692eabf39ed4ddd1d1af1
ggbotbox <oliver.lu@pa.com.sg>
2024-02-07 22:23:25 +0800

    add iwow models

---
+++ src/main/java/com/pabu5h/evs2/model/MeterReading3pIwowModel.java
@@ -0,0 +1,61 @@
+package com.pabu5h.evs2.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "meter_reading_iwow")
+public class MeterReading3pIwowModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "item_name")
+    private String itemName;
+
+    @Column(name = "item_sn")
+    private String itemSn;
+
+    @Column(name = "dt")
+    private LocalDateTime dt;
+
+    @Column(name = "val")
+    private Double val;
+
+    @Column(name = "ref")
+    private String ref;
+
+
+    @Column(name = "real_power")
+    private Double realPower;
+
+    @Column(name = "pf_l1")
+    private Double pfL1;
+    @Column(name = "pf_l2")
+    private Double pfL2;
+    @Column(name = "pf_l3")
+    private Double pfL3;
+
+    @Column(name = "current_l1")
+    private Double currentL1;
+    @Column(name = "current_l2")
+    private Double currentL2;
+    @Column(name = "current_l3")
+    private Double currentL3;
+
+    @Column(name = "voltage_l1")
+    private Double voltageL1;
+    @Column(name = "voltage_l2")
+    private Double voltageL2;
+    @Column(name = "voltage_l3")
+    private Double voltageL3;
+}
---
+++ src/main/java/com/pabu5h/evs2/model/MeterReadingBtuIwowModel.java
@@ -0,0 +1,54 @@
+package com.pabu5h.evs2.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "meter_reading_iwow")
+public class MeterReadingBtuIwowModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "item_name")
+    private String itemName;
+
+    @Column(name = "item_sn")
+    private String itemSn;
+
+    @Column(name = "dt")
+    private LocalDateTime dt;
+
+    @Column(name = "val")
+    private Double val;
+
+    @Column(name = "ref")
+    private String ref;
+
+
+    @Column(name = "flow")
+    private Double flow;
+
+    @Column(name = "power")
+    private Double power;
+
+    @Column(name = "volume")
+    private Double volume;
+
+    @Column(name = "forward_temp")
+    private Double forwardTemp;
+    @Column(name = "return_temp")
+    private Double returnTemp;
+    @Column(name = "temp_diff")
+    private Double tempDiff;
+
+}
