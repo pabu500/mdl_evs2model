a6d1d1b53f03ed64b4f333d4ca5ab7654fe0f642
pa596 <oliver.lu@pa.com.sg>
2024-02-08 13:55:00 +0800

    misc

--- src/main/java/com/pabu5h/evs2/model/MeterReadingIwowRepo.java
+++ src/main/java/com/pabu5h/evs2/model/MeterReadingIwowRepo.java
@@ -1,12 +1,12 @@
-//package com.pabu5h.evs2.model;
-//
-////Spring Boot 2.5+
-//
-//import org.springframework.data.repository.CrudRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface MeterReadingIwowRepo extends CrudRepository<MeterReadingIwowModel, Long> {
-////Spring Boot 3.0+
-////ListCrudRepository<MeterReadingModel, Long> {
-//}
+package com.pabu5h.evs2.model;
+
+//Spring Boot 2.5+
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MeterReadingIwowRepo extends CrudRepository<MeterReadingIwowModel, Long> {
+//Spring Boot 3.0+
+//ListCrudRepository<MeterReadingModel, Long> {
+}
