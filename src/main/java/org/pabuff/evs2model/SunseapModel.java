d500f8b7d7fb87e55a7be11cfd92ef42ae2c31f4
pa621bu500 <pa621bu500@gmail.com>
2024-09-05 12:12:06 +0800

    update to nullable

--- src/main/java/org/pabuff/evs2model/SunseapModel.java
+++ src/main/java/org/pabuff/evs2model/SunseapModel.java
@@ -26,151 +26,151 @@ public class SunseapModel {
     @Column(name = "substation_id")
     private Integer substationId;
 
-    @Column(name = "timestamp", nullable = false)
+    @Column(name = "timestamp")
     private LocalDateTime dt;
 
-    @Column(name = "j_01_sppg_cb_in", nullable = false)
+    @Column(name = "j_01_sppg_cb_in")
     private int j01SppgCbIn;
 
-    @Column(name = "j_02_cb_out", nullable = false)
+    @Column(name = "j_02_cb_out")
     private int j02CbOut;
 
-    @Column(name = "j_03_cb_out", nullable = false)
+    @Column(name = "j_03_cb_out")
     private int j03CbOut;
 
-    @Column(name = "j_01_tcs_in", nullable = false)
+    @Column(name = "j_01_tcs_in")
     private int j01TcsIn;
 
-    @Column(name = "j_01_sf6_gas_pressure_low_in", nullable = false)
+    @Column(name = "j_01_sf6_gas_pressure_low_in")
     private int j01Sf6GasPressureLowIn;
 
-    @Column(name = "j_01_ctl_in", nullable = false)
+    @Column(name = "j_01_ctl_in")
     private int j01CtlIn;
 
-    @Column(name = "j_01_differential_trip_in", nullable = false)
+    @Column(name = "j_01_differential_trip_in")
     private int j01DifferentialTripIn;
 
-    @Column(name = "j_01_protection_trip_in", nullable = false)
+    @Column(name = "j_01_protection_trip_in")
     private int j01ProtectionTripIn;
 
-    @Column(name = "j_01_bat_common_alarm_in", nullable = false)
+    @Column(name = "j_01_bat_common_alarm_in")
     private int j01BatCommonAlarmIn;
 
-    @Column(name = "J_01_intertrip_from_sppg_feeder_in", nullable = false)
+    @Column(name = "J_01_intertrip_from_sppg_feeder_in")
     private int j01IntertripFromSppgFeederIn;
 
-    @Column(name = "j_01_close_permissive_from_sppg_feeder_in", nullable = false)
+    @Column(name = "j_01_close_permissive_from_sppg_feeder_in")
     private int j01ClosePermissiveFromSppgFeederIn;
 
-    @Column(name = "j_02_tcs_out", nullable = false)
+    @Column(name = "j_02_tcs_out")
     private int j02TcsOut;
 
-    @Column(name = "j_02_sf6_gas_pressure_low_out", nullable = false)
+    @Column(name = "j_02_sf6_gas_pressure_low_out")
     private int j02Sf6GasPressureLowOut;
 
-    @Column(name = "j_02_ctl_out", nullable = false)
+    @Column(name = "j_02_ctl_out")
     private int j02CtlOut;
 
-    @Column(name = "j_02_protection trip_out", nullable = false)
+    @Column(name = "j_02_protection trip_out")
     private int j02ProtectionTripOut;
 
-    @Column(name = "j_02_tx_temp_alarm_out", nullable = false)
+    @Column(name = "j_02_tx_temp_alarm_out")
     private int j02TxTempAlarmOut;
 
-    @Column(name = "j_02_tx_pressure_alarm_out", nullable = false)
+    @Column(name = "j_02_tx_pressure_alarm_out")
     private int j02TxPressureAlarmOut;
 
-    @Column(name = "j_02_tx_temp_trip_out", nullable = false)
+    @Column(name = "j_02_tx_temp_trip_out")
     private int j02TxTempTripOut;
 
-    @Column(name = "j_02_tx_low_oil_trip_out", nullable = false)
+    @Column(name = "j_02_tx_low_oil_trip_out")
     private int j02TxLowOilTripOut;
 
-    @Column(name = "j_02_tx_pressure_trip_out", nullable = false)
+    @Column(name = "j_02_tx_pressure_trip_out")
     private int j02TxPressureTripOut;
 
-    @Column(name = "j_02_tx_pressure_relief_trip_out", nullable = false)
+    @Column(name = "j_02_tx_pressure_relief_trip_out")
     private int j02TxPressureReliefTripOut;
 
-    @Column(name = "j_03_tcs_out", nullable = false)
+    @Column(name = "j_03_tcs_out")
     private int j03TcsOut;
 
-    @Column(name = "j_03_sf6_gas_pressure_low_out", nullable = false)
+    @Column(name = "j_03_sf6_gas_pressure_low_out")
     private int j03Sf6GasPressureLowOut;
 
-    @Column(name = "j_03_ctl_out", nullable = false)
+    @Column(name = "j_03_ctl_out")
     private int j03CtlOut;
 
-    @Column(name = "j_03_protection_trip_out", nullable = false)
+    @Column(name = "j_03_protection_trip_out")
     private int j03ProtectionTripOut;
 
-    @Column(name = "j_03_tx_temp_alarm_out", nullable = false)
+    @Column(name = "j_03_tx_temp_alarm_out")
     private int j03TxTempAlarmOut;
 
-    @Column(name = "j_03_tx_pressure_alarm_out", nullable = false)
+    @Column(name = "j_03_tx_pressure_alarm_out")
     private int j03TxPressureAlarmOut;
 
-    @Column(name = "j_03_tx_temp_trip_out", nullable = false)
+    @Column(name = "j_03_tx_temp_trip_out")
     private int j03TxTempTripOut;
 
-    @Column(name = "j_03_tx_low_oil_trip_out", nullable = false)
+    @Column(name = "j_03_tx_low_oil_trip_out")
     private int j03TxLowOilTripOut;
 
-    @Column(name = "j_03_tx_pressure_trip_out", nullable = false)
+    @Column(name = "j_03_tx_pressure_trip_out")
     private int j03TxPressureTripOut;
 
-    @Column(name = "j_03_tx_pressure_relief_trip_out", nullable = false)
+    @Column(name = "j_03_tx_pressure_relief_trip_out")
     private int j03TxPressureReliefTripOut;
 
-    @Column(name = "cctv_alarm_1", nullable = false)
-    private double cctvAlarm1;
+    @Column(name = "cctv_alarm_1")
+    private Double cctvAlarm1;
 
-    @Column(name = "temp_sensor_1", nullable = false)
-    private double tempSensor1;
+    @Column(name = "temp_sensor_1")
+    private Double tempSensor1;
 
-    @Column(name = "temp_sensor_2", nullable = false)
-    private double tempSensor2;
+    @Column(name = "temp_sensor_2")
+    private Double tempSensor2;
 
-    @Column(name = "pyranometer", nullable = false)
-    private double pryanoMeter;
+    @Column(name = "pyranometer")
+    private Double pryanoMeter;
 
-    @Column(name = "v_l12", nullable = false)
-    private double vL12;
+    @Column(name = "v_l12")
+    private Double vL12;
 
-    @Column(name = "v_l23", nullable = false)
-    private double vL23;
+    @Column(name = "v_l23")
+    private Double vL23;
 
-    @Column(name = "v_l31", nullable = false)
-    private double vL31;
+    @Column(name = "v_l31")
+    private Double vL31;
 
-    @Column(name = "c_l1", nullable = false)
-    private double cL1;
+    @Column(name = "c_l1")
+    private Double cL1;
 
-    @Column(name = "c_l2", nullable = false)
-    private double cL2;
+    @Column(name = "c_l2")
+    private Double cL2;
 
-    @Column(name = "c_l3", nullable = false)
-    private double cL3;
+    @Column(name = "c_l3")
+    private Double cL3;
 
-    @Column(name = "a_p", nullable = false)
-    private double aP;
+    @Column(name = "a_p")
+    private Double aP;
 
-    @Column(name = "re_p", nullable = false)
-    private double reP;
+    @Column(name = "re_p")
+    private Double reP;
 
-    @Column(name = "pf", nullable = false)
-    private double pf;
+    @Column(name = "pf")
+    private Double pf;
 
-    @Column(name = "freq", nullable = false)
-    private double freq;
+    @Column(name = "freq")
+    private Double freq;
 
-    @Column(name = "kwh_delivery", nullable = false)
-    private double kwhDelivery;
+    @Column(name = "kwh_delivery")
+    private Double kwhDelivery;
 
-    @Column(name = "kwh_received", nullable = false)
-    private double kwhReceived;
+    @Column(name = "kwh_received")
+    private Double kwhReceived;
 
-    @Column(name = "created_timestamp", nullable = false)
+    @Column(name = "created_timestamp")
     private LocalDateTime createdTimestamp;
 
     @Column(name = "updated_timestamp")
